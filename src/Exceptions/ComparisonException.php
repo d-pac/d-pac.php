05c6c22c045e9febc2bc25d1830d37cf366529a8
William <william@lunargravity.be>
2017-10-18 13:35:30 +0200

    Added custom Exceptions for use in ComparativeSelection class

---
+++ src/Exceptions/ComparisonException.php
@@ -0,0 +1,12 @@
+<?php
+namespace Dpac\Dpac\Exception;
+
+/**
+ * Comparison Exception
+ *
+ * @author William Blommaert <william@lunargravity.be>
+ */
+class ComparisonException extends \Exception
+{
+
+}
---
+++ src/Exceptions/SelectionException.php
@@ -0,0 +1,12 @@
+<?php
+namespace Dpac\Dpac\Exception;
+
+/**
+ * Selection Exception
+ *
+ * @author William Blommaert <william@lunargravity.be>
+ */
+class SelectionException extends \Exception
+{
+
+}
