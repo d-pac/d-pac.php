f0ecbede828346c2af0b04d3497ca9ccf8bd8fff
William <william@lunargravity.be>
2017-10-20 13:16:03 +0200

    Implemented remaining Estimation function ports

--- src/Estimation.php
+++ src/Estimation.php
@@ -1,6 +1,9 @@
 <?php
 namespace Dpac\Dpac;
 
+use Dpac\Dpac\Functions\Pm as Pm;
+use Dpac\Dpac\Util as Util;
+
 /**
  * Estimation Class
  *
@@ -28,8 +31,8 @@ class Estimation
 
         $valueObject = [
             'id' => $item['id'],
-            'selected' => 0,
-            'compared' => 0,
+            'selectedNum' => 0,
+            'comparedNum' => 0,
             'ability' => $item['ability'],
             'se' => $item['se'],
             'ranked' => $item['ranked'],
@@ -38,5 +41,85 @@ class Estimation
         $lookup['objectsById'][$id] = $valueObject;
 
         return $valueObject;
+    }
+
+    /**
+     * Increments the comparedNum property of an object array
+     * and if it's the first comparison for an unranked item also
+     * initializes its abilities and stores it in the unranked lookup array
+     *
+     * @throws \InvalidArgumentException
+     * @param $lookup
+     * @param $id
+     */
+    public static function prepValuesOnFirstComparison(&$lookup, $id)
+    {
+        $object = self::getOrCreate($lookup, $id);
+
+        $object['comparedNum']++;
+
+        if ($object['comparedNum'] === 1 && !$object['ranked']) {
+            $object['ability'] = 0;
+            $object['se'] = 0;
+
+            if (empty($lookup['unrankedById'])) {
+                throw new \InvalidArgumentException('Expected lookup to contain sub-array unrankedById');
+            }
+
+            $lookup['unrankedById'][$id] = $object;
+        }
+    }
+
+    /**
+     * Conditional maximum likelihood
+     *
+     * @param $lookup
+     * @param $comparisons
+     * @param $iteration
+     */
+    public static function CML(&$lookup, $comparisons, $iteration)
+    {
+        if (empty($lookup['unrankedById'])) {
+            throw new \InvalidArgumentException('Expected lookup to contain sub-array unrankedById');
+        }
+
+        $previousUnranked = $lookup['unrankedById'];
+
+        $ids = array_keys($lookup['unrankedById']);
+
+        foreach ($ids as $id) {
+            $current = $lookup['objById'][$id];
+            $prev = $previousUnranked[$id];
+
+            $expected = array_reduce(
+                $comparisons,
+                function ($memo, $comparison) use ($id, $previousUnranked, $lookup, $prev) {
+
+                    $filteredIds = array_filter([$comparison['a'], $comparison['b']], function ($value) use ($id) {
+
+                        return $value == $id;
+                    });
+
+                    if ($comparison['selected'] && count($filteredIds) === 1) {
+                        $opponent = $previousUnranked[$filteredIds[0]] || $lookup['objById'][$filteredIds[0]];
+                        $memo['value'] += Pm::rasch($prev['ability'], $opponent['ability']);
+                        $memo['info'] += Pm::fisher($prev['ability'], $opponent['ability']);
+                    }
+
+                    return $memo;
+                },
+                ['value' => 0, 'info' => 0]
+            );
+
+            if ($iteration > 0) {
+                $current['ability'] = Util::clamp(
+                    $current['ability'] + (($current['selectedNum'] - $expected['value']) / $expected['info']),
+                    -10,
+                    10
+                );
+            } else {
+                $current['se'] = Util::clamp(1 / sqrt($expected['info']), -200, 200);
+            }
+        }
     }
 }
