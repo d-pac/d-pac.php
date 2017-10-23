ecfcc6148d3fa127e244abdbefb70689271e34e4
William <william@lunargravity.be>
2017-10-23 09:16:41 +0200

    Translated unit tests for Estimation

--- tests/EstimationTest.php
+++ tests/EstimationTest.php
@@ -8,6 +8,55 @@ namespace Dpac\Dpac;
  */
 class EstimationTest extends \PHPUnit_Framework_TestCase
 {
+    protected $mixedRankComparisons;
+    protected $mixedRankRepresentations;
+    protected $mixedRankResults;
+
+    protected $noRankedComparisons;
+    protected $noRankedRepresentations;
+    protected $noRankedResults;
+
+    protected $realComparisons;
+    protected $realRepresentations;
+    protected $realResults;
+
+    protected $nulledAbilityRepresentations;
+    protected $uncomparedRepresentations;
+
+    /**
+     * Initialize all our test arrays
+     */
+    public function setUp()
+    {
+        $path = __DIR__ . '/fixtures/';
+
+        $this->mixedRankComparisons = $this->getJsonContent($path . 'mixedRankedComparisons.json');
+        $this->mixedRankRepresentations = $this->getJsonContent($path . 'mixedRankedRepresentations.json');
+        $this->mixedRankResults = $this->getJsonContent($path . 'mixedRankedResults.json');
+
+        $this->noRankedComparisons = $this->getJsonContent($path . 'noRankedComparisons.json');
+        $this->noRankedRepresentations = $this->getJsonContent($path . 'noRankedRepresentations.json');
+        $this->noRankedResults = $this->getJsonContent($path . 'noRankedResults.json');
+
+        $this->realComparisons = $this->getJsonContent($path . 'realComparisons.json');
+        $this->realRepresentations = $this->getJsonContent($path . 'realRepresentations.json');
+        $this->realResults = $this->getJsonContent($path . 'realResults.json');
+
+        $this->nulledAbilityRepresentations = $this->getJsonContent($path . 'nulledAbilityRepresentations.json');
+        $this->uncomparedRepresentations = $this->getJsonContent($path . 'uncomparedRepresentations.json');
+    }
+
+    /**
+     * Fetch the body of a json file for a given path
+     *
+     * @param $path
+     * @return mixed
+     */
+    private function getJsonContent($path)
+    {
+        return json_decode(file_get_contents($path), true);
+    }
+
     /**
      * Converts representations to a usable format
      *
@@ -18,9 +67,9 @@ class EstimationTest extends \PHPUnit_Framework_TestCase
     {
         return array_map(function ($item) {
             return [
-                'id' => $item['id'],
-                'ability' => $item['ability']['value'],
-                'se' => $item['ability']['se'],
+                'id' => $item['_id'],
+                'ability' => isset($item['ability']['value']) ? $item['ability']['value'] : null,
+                'se' => isset($item['ability']['se']) ? $item['ability']['se'] : null,
                 'ranked' => $item['rankType'] !== 'to rank'
             ];
         }, $representations);
@@ -51,7 +100,6 @@ class EstimationTest extends \PHPUnit_Framework_TestCase
     private function mapToLookupHash($memo, $r)
     {
         $memo[$r['id']] = $r;
-
         return $memo;
     }
 
@@ -70,5 +118,192 @@ class EstimationTest extends \PHPUnit_Framework_TestCase
             'se' => $o['se'],
             'ranked' => $o['ranked']
         ];
+    }
+
+    /**
+     * If not all representations are to rank
+     * it should equal R generated results if
+     * all data are provided at once
+     * @group rasch
+     * @group raschfail
+     */
+    public function testNotAllRankEqualsRGeneratedResultsIfAllProvided()
+    {
+        $expected = array_filter($this->mixedRankResults, function ($item) {
+            return $item['rankType'] == 'to rank';
+        });
+
+        $expected = $this->convertRepresentations($expected);
+        $expected = array_reduce($expected, [$this, 'maptoLookupHash'], []);
+
+        $representations = $this->convertRepresentations($this->mixedRankRepresentations);
+        $comparisons = $this->convertComparisons($this->mixedRankComparisons);
+
+        $actual = Estimation::estimate(['comparisons' => $comparisons, 'items' => $representations]);
+        $actual = array_map([$this, 'prepResult'], $actual);
+        $actual = array_reduce($actual, [$this, 'maptoLookupHash'], []);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * If not all representations are to rank
+     * it should equal R generated results if
+     * estimates were first based on a part of the data
+     * @group rasch
+     * @group raschfail
+     */
+    public function testNotAllRankEqualsRGeneratedResultsFirstBasedPartOfData()
+    {
+        $expected = array_filter($this->mixedRankResults, function ($item) {
+            return $item['rankType'] == 'to rank';
+        });
+
+        $expected = $this->convertRepresentations($expected);
+        $expected = array_reduce($expected, [$this, 'maptoLookupHash'], []);
+
+        $representations = $this->convertRepresentations($this->mixedRankRepresentations);
+        $comparisons = $this->convertComparisons($this->mixedRankComparisons);
+
+        $firstComparisons = array_rand($comparisons, count($comparisons) / 2);
+        Estimation::estimate(['comparisons' => $firstComparisons, 'items' => $representations]);
+
+        $actual = Estimation::estimate(['comparisons' => $comparisons, 'items' => $representations]);
+        $actual = array_map([$this, 'prepResult'], $actual);
+        $actual = array_reduce($actual, [$this, 'maptoLookupHash'], []);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * If all representations are to rank
+     * it should equal R generated results if
+     * all data are provided at once
+     * @group rasch
+     * @group raschfail
+     */
+    public function testAllRankEqualsRGeneratedResultsIfAllProvided()
+    {
+        $expected = $this->convertRepresentations($this->noRankedResults);
+        $expected = array_reduce($expected, [$this, 'maptoLookupHash'], []);
+
+        $representations = $this->convertRepresentations($this->noRankedRepresentations);
+        $comparisons = $this->convertComparisons($this->noRankedComparisons);
+
+        $actual = Estimation::estimate(['comparisons' => $comparisons, 'items' => $representations]);
+        $actual = array_map([$this, 'prepResult'], $actual);
+        $actual = array_reduce($actual, [$this, 'maptoLookupHash'], []);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * If all representations are to rank
+     * it should equal R generated results if
+     * estimates were first based on a part of the data
+     * @group rasch
+     * @group raschfail
+     */
+    public function testAllRankEqualsRGeneratedResultsFirstBasedPartOfData()
+    {
+        $expected = $this->convertRepresentations($this->noRankedResults);
+        $expected = array_reduce($expected, [$this, 'maptoLookupHash'], []);
+
+        $representations = $this->convertRepresentations($this->noRankedRepresentations);
+        $comparisons = $this->convertComparisons($this->noRankedComparisons);
+
+        $firstComparisons = array_rand($comparisons, count($comparisons) / 2);
+        Estimation::estimate(['comparisons' => $comparisons, 'items' => $representations]);
+
+        $actual = Estimation::estimate(['comparisons' => $comparisons, 'items' => $representations]);
+        $actual = array_map([$this, 'prepResult'], $actual);
+        $actual = array_reduce($actual, [$this, 'maptoLookupHash'], []);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * If real data
+     * it should equal R generated results
+     * @group rasch
+     * @group raschfail
+     */
+    public function testRealDataEqualsRGeneratedResults()
+    {
+        $expected = $this->convertRepresentations($this->realResults);
+        $expected = array_reduce($expected, [$this, 'maptoLookupHash'], []);
+
+        $representations = $this->convertRepresentations($this->realRepresentations);
+        $comparisons = $this->convertComparisons($this->realComparisons);
+
+        $actual = Estimation::estimate(['comparisons' => $comparisons, 'items' => $representations]);
+        $actual = array_map([$this, 'prepResult'], $actual);
+        $actual = array_reduce($actual, [$this, 'maptoLookupHash'], []);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * If nulled abilities
+     * it should equals R generated results
+     * @group rasch
+     * @group raschfail
+     */
+    public function testNulledAbilitiesEqualsRGeneratedResults()
+    {
+        $expected = $this->convertRepresentations($this->noRankedResults);
+        $expected = array_reduce($expected, [$this, 'maptoLookupHash'], []);
+
+        $representations = $this->convertRepresentations($this->nulledAbilityRepresentations);
+        $comparisons = $this->convertComparisons($this->noRankedComparisons);
+
+        $actual = Estimation::estimate(['comparisons' => $comparisons, 'items' => $representations]);
+        $actual = array_map([$this, 'prepResult'], $actual);
+        $actual = array_reduce($actual, [$this, 'maptoLookupHash'], []);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * If uncompared representations
+     * it should ignore them
+     * @group rasch
+     * @group raschfails
+     */
+    public function testUncomparedRepresentationsShouldIgnoreThem()
+    {
+        $expected = $this->convertRepresentations($this->noRankedResults);
+        $expected = array_reduce($expected, [$this, 'maptoLookupHash'], []);
+
+        $representations = $this->convertRepresentations($this->uncomparedRepresentations);
+        $comparisons = $this->convertComparisons($this->noRankedComparisons);
+
+        $actual = Estimation::estimate(['comparisons' => $comparisons, 'items' => $representations]);
+        $actual = array_map([$this, 'prepResult'], $actual);
+        $actual = array_reduce($actual, [$this, 'maptoLookupHash'], []);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * If map of items
+     * it should accept them and output in the same type
+     * @group rasch
+     * @group raschfails
+     */
+    public function testMapOfItemsShouldBeAcceptedAndOutputSameType()
+    {
+        $expected = $this->convertRepresentations($this->noRankedResults);
+        $expected = array_reduce($expected, [$this, 'maptoLookupHash'], []);
+
+        $representations = $this->convertRepresentations($this->uncomparedRepresentations);
+        $representations = array_reduce($representations, [$this, 'maptoLookupHash'], []);
+
+        $comparisons = $this->convertComparisons($this->noRankedComparisons);
+
+        $actual = Estimation::estimate(['comparisons' => $comparisons, 'items' => $representations]);
+        $actual = array_map([$this, 'prepResult'], $actual);
+
+        $this->assertEquals($expected, $actual);
     }
 }
